4e1e37c9fdb4af7c3ce9b2fe7b104d0c9edc3aa4
b08x <rwpannick@gmail.com>
2023-02-25 01:38:42 -0500

    package list successfull, still to test

--- ans.rb
+++ ans.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'colorize'
 require 'ruport'
 require 'yaml'
 
@@ -8,48 +9,96 @@ ANSIBLE_HOME = File.join(Dir.home, "Workspace", "syncopated")
 
 module Soundbot
 
-  class Config
+  class Packages
+    attr_accessor :packages
 
-    def self.load_vars(varfile)
+    def initialize()
+      @packages = Hash.new()
+      return self
+    end
+
+    def load_vars(varfile)
       YAML.load_file(varfile)
     end
 
     def self.get(attribute)
-      return Config.load_config[attribute]
+      return Config.load_vars[attribute]
     end
 
-    def self.set
+    def self.set(varfile)
       File.open(CONFIG, "w") { |file| file.write $CONFIG_DIR.to_yaml }
+    end
+
+    # https://stackoverflow.com/a/23521624
+    def self.flatten_hash(hash)
+      hash.each_with_object({}) do |(k, v), h|
+        if v.is_a? Hash
+          flatten_hash(v).map do |h_k, h_v|
+            h["#{k}.#{h_k}".to_sym] = h_v
+          end
+        else
+          h[k] = v
+        end
+       end
     end
 
   end
 
+
+  class PackageManager
+    def initialize(distro)
+      case distro
+      when Archlinux
+        @packagemanager = ""
+
+      end
+    end
+
+  end
+
+
 end
 
 
-# allpackages = []
-#
-# varfiles = Dir.glob(File.join(ANSIBLE_HOME, "roles", "distro", "vars", "packages", "*.yml"))
+# distro = ARGV[0]
+distro = "Archlinux"
+
+allpackages = []
+
+varfiles = Dir.glob(File.join(ANSIBLE_HOME, "playbooks", "vars", "packages", distro, "*.yml"))
+
+varfiles.each do |varfile|
+  pkgs = Soundbot::Packages.new
+
+  x = pkgs.load_vars(varfile)
+
+  Soundbot::Packages.flatten_hash(x).each do |kv|
+    allpackages << kv.drop(1).flatten
+  end
+
+end
+
+allpackages.flatten!
+
+currently_installed = `paru -Q | awk '{print $1}'`.split("\n")
+
+packages_to_install = allpackages - currently_installed
+
+p packages_to_install.join(" ")
+
+#`paru -S #{packages_to_install.join(" ")}`
 
 
-require 'colorize'
+# puts "#{packages}".blue
+# puts 'Bold cyan on blue text'.cyan.on_blue.bold
+# puts "This is #{"fancy".red} text"
 
-puts "Blue text".blue
-puts 'Bold cyan on blue text'.cyan.on_blue.bold
-puts "This is #{"fancy".red} text"
-
-exit(0)
-# varfiles.each do |varfile|
-#
-#   allpackages << Soundbot::Config.load_vars(varfile)
-#
-# end
 #
 # p allpackages
 # #
 # packages = Soundbot::Config.get("packages")
 #
-# table = Ruport::Data::Table.new
+table = Ruport::Data::Table.new
 #
 # table.column_names = %w[Key Value]
 #
